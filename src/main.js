7fc3e230242282f51daf0bb7836acf83aca88f28
HeightenCM <hasconstantin@proton.me>
2025-12-14 12:43:43 +0200

    Add PrimeVue and new Readme

--- src/main.js
+++ src/main.js
@@ -6,9 +6,21 @@ import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 
+import PrimeVue from 'primevue/config'
+import Aura from '@primeuix/themes/aura'
+
+import Button from 'primevue/button'
+
 const app = createApp(App)
 
 app.use(createPinia())
 app.use(router)
+app.use(PrimeVue, {
+  theme: {
+    preset: Aura,
+  },
+})
+
+app.component('Button', Button)
 
 app.mount('#app')
